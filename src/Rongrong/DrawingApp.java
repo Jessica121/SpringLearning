aa48477851d6405f9c4f743c11e1c27515baa472
Jessica121 <janesis@hotmail.com>
2017-09-14 19:08:02 -0400

    setter injection using tag property, attribute name and value and constructor injection using constructor-arg type / index

--- src/Rongrong/DrawingApp.java
+++ src/Rongrong/DrawingApp.java
@@ -1,19 +1,21 @@
 package Rongrong;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.core.io.FileSystemResource;
 
-@SuppressWarnings("deprecation")
+//@SuppressWarnings("deprecation")
 public class DrawingApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		Triangle tri = new Triangle();
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle tri = (Triangle) factory.getBean("tri");
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle tri = (Triangle) context.getBean("tri");
 		tri.draw();
-	
+	 
 	}
 
 }
--- src/Rongrong/Triangle.java
+++ src/Rongrong/Triangle.java
@@ -1,7 +1,40 @@
 package Rongrong;
 
 public class Triangle {
+	//member variable
+	private String type;
+	private int height;
+	
+	public int getHeight() {
+		return height;
+	}
+//	constructor 1
+	public Triangle(String t){
+		this.type = t;
+	}
+	
+//	constructor 3
+	public Triangle(int h){
+		this.height = h;
+	}
+	
+//	constructor 2
+	public Triangle(String t,int h){
+		this.type = t;
+		this.height = h;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+	
+	
+
 	public void draw(){
-		System.out.println("Triangle drawn");
+		System.out.println("Triangle height and type ->" + getHeight() + getType());
 	}
 }
